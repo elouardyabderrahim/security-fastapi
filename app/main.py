797ed18a6386c37519b857b87ef85849b3180085
unknown <elouardy.abderrahim07@gmail.com>
2024-08-06 17:57:45 +0100

    get current user

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ from typing import Annotated
 from fastapi.security import OAuth2PasswordBearer
 
 
+from pydantic import BaseModel
+
 app=FastAPI()
+
 
 
 OAuth2_scheme=OAuth2PasswordBearer(tokenUrl='token')
@@ -14,10 +17,31 @@ OAuth2_scheme=OAuth2PasswordBearer(tokenUrl='token')
 #  then it would refer to https://example.com/token.
 
 
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
 
+
+def fake_decode_token(token):
+    return User(
+        username=token +"fake_decoded",
+        email="john@example.com",
+        full_name="John Doe"
+
+    )
+
+print(OAuth2_scheme.__dict__)
+
+async def get_correct_user(token : Annotated [str,Depends(OAuth2_scheme)]):
+    
+    user=fake_decode_token(token)
+    return user
 
 
 @app.get("/user/me")
-async def read_utems(token:Annotated[str,Depends(OAuth2_scheme)]):
-    return {"token":token}
+async def read_user_me(current_user:Annotated[User,Depends(get_correct_user)]):
+
+    return current_user
     
