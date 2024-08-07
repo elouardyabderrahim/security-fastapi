3ed4d33f1cb7f7bf66bfd5e375f99cee9b984a29
unknown <elouardy.abderrahim07@gmail.com>
2024-08-07 17:34:20 +0100

    context for hashing and verifying password

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from typing import Annotated
 from fastapi.security import OAuth2PasswordBearer
 
 
-from pydantic import BaseModel
 
 
 #PassLib is a great Python package to handle password hashes.
@@ -25,6 +24,17 @@ fake_users_db = {
     }
 }
 
+# Create a PassLib "context". This is what will be used to hash and verify passwords.
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password,hashed_password):
+    
+    return pwd_context.verify(plain_password,hashed_password) 
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 
 
@@ -39,11 +49,6 @@ OAuth2_scheme=OAuth2PasswordBearer(tokenUrl='token')
 #  then it would refer to https://example.com/token.
 
 
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
 
 
 def fake_decode_token(token):
